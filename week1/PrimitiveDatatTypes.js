fca28e0480d79c9c8fc4e58ce5dd3fe3480b6cc7
ponnimay <ponnimay@gmail.com>
2025-12-18 09:54:43 +0530

    Add files via upload

---
+++ week1/PrimitiveDatatTypes.js
@@ -0,0 +1,11 @@
+let firstname = "Ponni"
+let companyName = "XYZ"
+let mobileNumber = 9078905678
+let isAutomation = true
+let hasplaywright
+
+console.log("The First Name : "+firstname+" and datatype is : "+ typeof firstname)
+console.log("The Company Name : "+companyName+" and its datatype is : "+ typeof companyName)
+console.log("The Mobile Number : "+mobileNumber+" and its datatype is : "+ typeof mobileNumber)
+console.log("The Automation : "+isAutomation+" and its datatype is : "+ typeof isAutomation)
+console.log("Has Playwright : "+hasplaywright+" and its datatype is : "+ typeof hasplaywright)
